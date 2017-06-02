bb0e5e7ad662fe6aa20109477a68d318ad7ecb77
PatelleLu <471569318@qq.com>
2017-06-02 11:02:25 +0800

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { CourseDetailsComponent } from './course-details/course-details.component';
 import { NewsComponent } from './news/news.component';
+import { CourseSynopsisComponent } from './course-synopsis/course-synopsis.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseDetailsComponent,
-    NewsComponent
+    NewsComponent,
+    CourseSynopsisComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/course-synopsis/course-synopsis.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-synopsis',
+  templateUrl: './course-synopsis.component.html',
+  styleUrls: ['./course-synopsis.component.scss']
+})
+export class CourseSynopsisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
